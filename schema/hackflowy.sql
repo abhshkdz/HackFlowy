294528708bf7838763d95647b6ef546c6fc3d98d
Abhishek Das <das.abhshk@gmail.com>
2013-04-06 02:10:20 +0530

    Update table; Add parent_id column

--- schema/hackflowy.sql
+++ schema/hackflowy.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 30, 2013 at 07:12 PM
+-- Generation Time: Apr 06, 2013 at 02:09 AM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.6-1ubuntu1.2
 
@@ -23,17 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `parents`
---
-
-CREATE TABLE IF NOT EXISTS `parents` (
-  `task_id` int(10) NOT NULL,
-  `parent_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tasks`
 --
 
@@ -41,8 +30,9 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `content` varchar(1000) NOT NULL,
   `timestamp` int(12) NOT NULL,
+  `parent_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=96 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
