c5ac6085108343698714676c9efa8a48a2a507fe
floydpraveen <pdnsjce@gmail.com>
2013-08-22 17:46:31 +0530

    method to mark the model complete/ uncomplete

--- public/javascripts/models/task.js
+++ public/javascripts/models/task.js
@@ -10,8 +10,22 @@ Backbone
 
     defaults: {
       parent_id: '',
-      content: ''
+      content: '',
+      is_completed:'n'
     },
+
+    toggelCompletedStatus:function(isCompleted){
+    	var prev_isCompleted = isCompleted,
+    		self = this;
+    	this.save({'is_completed':isCompleted},
+    	{
+    		success:function(){},
+    		error:function(){
+    			//REVERT BACK ON ERROR
+    			self.set({'is_completed':prev_isCompleted});
+    		}
+    	})
+    }
 
  });
 
