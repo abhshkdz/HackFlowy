84a86facfcda7661ade70add238cfc89c65a1294
Is Isilon <wassname@wassname.org>
2016-01-29 13:08:18 +0800

    Add defaults to model

--- db/seed/initial_tasks.js
+++ db/seed/initial_tasks.js
@@ -7,7 +7,7 @@ Tasks.destroy({where: {}}).then(function() {
         {content: 'Welcome to HackFlowy!', isCompleted: false},
         {content: 'An open-source WorkFlowy clone', isCompleted: false},
         {content: 'Built using Backbone + Socket.IO', isCompleted: false},
-        {content: 'I pulled this together in a few hours to learn Backbone', isCompleted: false},
+        {content: 'Desyncr pulled this together in a few hours to learn Backbone', isCompleted: false},
         {content: 'Feel free to try it out and hack on it', isCompleted: false},
         {content: 'Good Luck!', isCompleted: false}
     ]);
