ed4641650b4850b7e99d327fa0ba3232ce0c756a
Is Isilon <wassname@wassname.org>
2016-01-30 07:26:13 +0800

    Fix for offline sync

--- public/javascripts/collections/list.js
+++ public/javascripts/collections/list.js
@@ -13,22 +13,30 @@ localforage,
 localforageBackbone
 ) {
 
-  var List = Backbone.Collection.extend({
+    var List = Backbone.Collection.extend({
 
-
-    model: Task,
-    offlineSync: Backbone.localforage.sync("tasks"),
-    /** switches sync between server and local databases **/
-    sync: function(){
+        model: Task,
+        offlineSync: Backbone.localforage.sync("tasks"),
+        /** switches sync between server and local databases **/
+        sync: function(){
+            //var self = this;
+            _localforageNamespace = this.offlineSync._localforageNamespace;
+            _localeForageKeyFn=this.offlineSync._localeForageKeyFn;
+            localforageKey = this.offlineSync._localeForageKeyFn;
             if (window.hackflowyOffline)
                 return this.offlineSync.apply(this, arguments);
             else
                 return Backbone.sync.apply(this, arguments);
-    },
+        },
 
-    url: '/tasks'
+        url: '/tasks'
 
- });
+    });
+
+    // a couple of vars backbone.localforage needs in the sync function
+    List.prototype.sync.localforage = List.prototype.offlineSync._localeForageKeyFn;
+    List.prototype.sync._localeForageKeyFn = List.prototype.offlineSync._localeForageKeyFn;
+    List.prototype.sync._localforageNamespace = List.prototype.offlineSync._localforageNamespace;
 
 return List;
 
