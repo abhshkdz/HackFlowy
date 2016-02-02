8afc3e949f655d106b23cb2096532c6b6526727a
Is Isilon <wassname@wassname.org>
2016-02-02 18:41:14 +0800

    Made db:seed fail gracefully

--- db/seed/initial_tasks.js
+++ db/seed/initial_tasks.js
@@ -1,10 +1,13 @@
 var config = require('config'),
+    sequelize = require("sequelize"),
     orm = require('../../orm').configure(config.get('database')),
     Tasks = require('../../db/models/task').instance(orm),
     demoData = require('../../public/javascripts/data/demo.json');
 
-Tasks.destroy({where: {}}).then(function() {
-    Tasks.bulkCreate(demoData);
+// Create tables if they dont exist
+Tasks.sync().then(function(){
+    // fill demo data if it doesn't exist
+    for (var i = 0; i < demoData.length; i++) {
+        Tasks.findOrCreate({where: demoData[i]});
+    }
 });
-
-orm.sync();
